f84a6e16c301d191fa630224fe82eb3f490cc672
Rahul Kumar <rkg06313@gmail.com>
2023-07-11 23:16:56 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
   await mongoose.connect(process.env.MONGO_URI,{
     dbName: "todoapi",
   })
-  .then(()=> console.log("Database connected"))
+  .then((c)=> console.log(`Database connected with ${c.connection.host}`))
   .catch((e)=> console.log(e))
   
   
